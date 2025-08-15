3da1999da2c4070ddbe71c2f9677095c410df0aa
Senko <152493636+helpful-fox-senko-san@users.noreply.github.com>
2025-08-15 13:49:44 +0000

    Fix resourcees path

--- Visibility/Properties/Resources.Designer.cs
+++ Visibility/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Visibility.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Visibility.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LopVisibility.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
